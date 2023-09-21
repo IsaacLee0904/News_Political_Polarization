44c6a1bb33aec93f00d83f3e1447cc26b1139153
Isaac Lee <hool19965401@gamil.com>
2023-09-21 15:02:03 +0800

    all task add log

--- task/data_initial_processing.py
+++ task/data_initial_processing.py
@@ -13,30 +13,33 @@ from utils.etl_utils import save_csv_to_db
 from utils.etl_utils import move_to_backup_folder
 from utils.etl_utils import crawl_news
 from utils.etl_utils import re_crawl_failed_news
+from utils.log_utils import set_logger
 
 def main():
 
     ''' setting configure '''
+    logger = set_logger()
     output_folder = os.path.join(project_root, "data", "raw_data")
     backup_folder = os.path.join(project_root, "data", "backup")
     db_path = os.path.join(project_root, 'data', 'db', 'news_political_polarization.sqlite')
 
     ''' main workflow '''          
     json_list = get_all_json()  
+    logger.info(f"Loaded {len(json_list)} JSON files for processing.")
 
     for data_json in json_list:
 
         file_name = '_'.join(os.path.splitext(os.path.basename(data_json))[0].split('_')[-2:])
-        print("{0} Loading : {1} {0}".format('-' * 40, file_name))
+        logger.info(f"Loading : {file_name}")
 
         # split out news source
         match = re.search(r'_(\w+)_(\w+).json$', data_json)
         if match:
             source = match.group(1)
             keyword = match.group(2)
-            print('News source : ' + str(source) + '_' + str(keyword))
+            logger.info(f'News source : {source}_{keyword}')
         else:
-            print("No match found!")
+            logger.warning("No match found for news source in filename.")
         
         # storage json to csv file
         with open(data_json, 'r' , encoding='utf-8') as read_file:
@@ -46,9 +49,9 @@ def main():
                 try:
                     data_list.append(json.loads(line))
                 except json.JSONDecodeError:
-                    print("Error decoding JSON from line:", line)
+                    logger.error(f"Error decoding JSON from line: {line}")
 
-            print('Size of data_list:', len(data_list))
+            logger.info(f'Size of data_list: {len(data_list)}')
 
             news_df = pd.DataFrame(data_list, columns = ['url' , 'title', 'category', 'up_datetime', 'content'])
 
@@ -60,20 +63,21 @@ def main():
             # reshape df 
             news_df = news_df[['source', 'keyword', 'title', 'category', 'up_datetime', 'content', 'url']]
             re_crawl_failed_news(news_df)
-            print(news_df.head())
+            logger.debug(news_df.head().to_string())
             
             # convert dataframe as csv file
             new_file_name = os.path.join(output_folder, file_name + ".csv")
             news_df.to_csv(new_file_name, index = False)
+            logger.info(f"Data saved to CSV file: {new_file_name}")
 
             # storage dataframe into database
             try:
                 save_csv_to_db(new_file_name, db_path)
                 move_to_backup_folder(data_json, backup_folder)
-                print(f"{data_json} has been moved to backup folder.")
+                logger.info(f"{data_json} has been moved to backup folder.")
                     
             except Exception as e:
-                print(f"Error processing {data_json}: {e}")
+                logger.error(f"Error processing {data_json}: {e}")
 
 if __name__ == '__main__':
     main()
--- task/setup_db.py
+++ task/setup_db.py
@@ -1,6 +1,13 @@
 import sys, os
 import sqlite3
 
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
+from utils.log_utils import set_logger
+
+logger = set_logger()
+
 # path of create database 
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_root)
@@ -9,8 +16,10 @@ db_path = os.path.join(project_root, 'data', 'db', 'news_political_polarization.
 # check if the database exist
 if not os.path.exists(os.path.dirname(db_path)):
     os.makedirs(os.path.dirname(db_path))
+    logger.info(f"Created directory: {os.path.dirname(db_path)}")
 
 # create / connect the database
+logger.info(f"Connecting to the database at: {db_path}")
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
@@ -18,6 +27,7 @@ cursor = conn.cursor()
 table_names = ['nuclear_power', 'ractopamine', 'alongside_elections', 'algal_reef']
 
 for table in table_names:
+    logger.info(f"Creating table '{table}' if it doesn't exist.")
     cursor.execute(f"""
         CREATE TABLE IF NOT EXISTS {table} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,6 +40,8 @@ for table in table_names:
             url TEXT
         )
     """)
+    logger.info(f"Table '{table}' has been set up.")
 
 conn.commit()
 conn.close()
+logger.info("Database connection has been closed.")
